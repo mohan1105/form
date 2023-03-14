b4b50c252687d6fd15b74814136db08547e85f62
mohan1105 <125288449+mohan1105@users.noreply.github.com>
2023-03-14 19:07:19 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,18 @@
+<?php 
+ $conn= new mysqli('localhost','root','','register');
+ $fname= $_POST['fname']; 
+ $lname= $_POST['lname']; 
+ $saddress= $_POST['saddress']; 
+ $gender= $_POST['gender']; 
+ $dob=$_POST['dob'];
+ $email= $_POST['email']; 
+ $mobile= $_POST['mobile']; 
+ $course= $_POST['course']; 
+
+ $sql = "INSERT INTO form(fname,lname,saddress,gender,dob,email,mobile,course) VALUES('$fname','$lname','$saddress','$gender','$dob','$email','$mobile','$course')";
+ $query= mysqli_query($conn,$sql);
+ if($query){
+    echo "<center> <p> REGISTRATION SUCESSFULLY</p></center>";
+    echo "<center> <button><a href='form.html'>GO TO HOME</a></button></center>";
+ }    
+ ?>
